a08b9f92921d948c1e14b5c9f665cef4735986e1
IssamElJaouhari <issamefar58@gmail.com>
2025-09-22 15:34:34 +0100

    add routes for about, contact, and projets pages

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,21 @@ connectdata()
 
 const app = express()
 
+app.get("/", (req, res) => {
+    res.end("<h1>HELLO BACKEND</h1>")
+})
 
+app.get("/about", (req, res) => {
+    res.end("<h1>about page</h1>")
+})
+
+app.get("/contact", (req, res) => {
+    res.end("<h1>contact page</h1>")
+})
+
+app.get("/projets", (req, res) => {
+    res.end("<h1>about page</h1>")
+})
 
 
 app.listen(7007, () => {
